02a9a14d39fe9d92c0ed35d5eff08fb1d759ce81
Christopher Pecoraro <chrispecoraro@gmail.com>
2016-09-01 09:50:35 +0200

    initial commit

---
+++ inc/glucosioedc.h
@@ -0,0 +1,23 @@
+#ifndef __glucosioedc_H__
+#define __glucosioedc_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "glucosioedc"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.glucosioedc"
+#endif
+
+#define EDJ_FILE "edje/glucosioedc.edj"
+#define GRP_MAIN "main"
+
+
+#endif /* __glucosioedc_H__ */
